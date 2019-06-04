3a5e733d4bf658a3317a2b84b8984f741e36d87d
Steefler <tymoteusz.przylucki@gmail.com>
2019-06-04 12:08:14 +0200

    Client_config.rb - update configuration

--- src/client/client_config.rb
+++ src/client/client_config.rb
@@ -4,13 +4,15 @@ require './src/config_loader.rb'
 class ClientConfigurationLoader < ConfigurationLoader
   attr_accessor :json_content, :server_addr, :server_port, :server_queue_name
 
+  # Initialization
   def initialize
     super()
     @server_addr = "127.0.0.1"
     @server_port = "5672"
     @server_queue_name = "rpc_queue"
   end
-
+  # Load configuration from file
+  # @param [String] file - Path to the file
   def load_configuration(pathFile)
     super(pathFile)
     unless @json_content.empty?
